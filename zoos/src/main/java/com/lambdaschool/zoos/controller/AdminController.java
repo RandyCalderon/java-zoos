4153a54458c2f6024d7ce5e02de6de5fe068251f
Randy Calderon <randycweb@gmail.com>
2019-02-27 00:43:16 -0800

    Added put endpoints for admin

--- zoos/src/main/java/com/lambdaschool/zoos/controller/AdminController.java
+++ zoos/src/main/java/com/lambdaschool/zoos/controller/AdminController.java
@@ -1,12 +1,18 @@
 package com.lambdaschool.zoos.controller;
 
+import com.lambdaschool.zoos.model.Animal;
+import com.lambdaschool.zoos.model.Telephone;
+import com.lambdaschool.zoos.model.Zoo;
 import com.lambdaschool.zoos.repository.AnimalRepository;
 import com.lambdaschool.zoos.repository.TelephoneRepository;
 import com.lambdaschool.zoos.repository.ZooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 
 @RestController
@@ -21,5 +27,66 @@ public class AdminController {
     @Autowired
     ZooRepository zoorepos;
 
+    // Post endpoints
+
+    // Post new zoo
+    @PostMapping("/zoos")
+    public Zoo createZoo(@RequestBody Zoo zoo) throws URISyntaxException {
+        return zoorepos.save(zoo);
+    }
+
+    // Post new phone
+    @PostMapping("/phones")
+    public Telephone createPhone(@RequestBody Telephone phone) throws URISyntaxException {
+        return phonerepos.save(phone);
+    }
+
+    // Post new animal
+    @PostMapping("/animals")
+    public Animal createAnimal(@RequestBody Animal animal) throws URISyntaxException {
+        return animalrepos.save(animal);
+    }
+
+    // Put endpoints
+    
+
+    // Update zoo by id
+    @PutMapping("/zoos/{id}")
+    public Zoo updateZooById(@RequestBody Zoo updatedZoo, @PathVariable long id) throws URISyntaxException {
+        Optional<Zoo> newZoo = zoorepos.findById(id);
+        if (newZoo.isPresent()) {
+            updatedZoo.setZooid(id);
+            zoorepos.save(updatedZoo);
+            return updatedZoo;
+        } else {
+            return null;
+        }
+    }
+
+    // Update phone by id
+    @PutMapping("/phones/{id}")
+    public Telephone updatePhoneById(@RequestBody Telephone updatedPhone, @PathVariable long id) throws URISyntaxException {
+        Optional<Telephone> newPhone = phonerepos.findById(id);
+        if (newPhone.isPresent()) {
+            updatedPhone.setPhoneid(id);
+            phonerepos.save(updatedPhone);
+            return updatedPhone;
+        } else {
+            return null;
+        }
+    }
+
+    // Update animal by id
+    @PutMapping("/animals/{id}")
+    public Animal updateAnimalById(@RequestBody Animal updatedAnimal, @PathVariable long id) throws URISyntaxException {
+        Optional<Animal> newAnimal = animalrepos.findById(id);
+        if (newAnimal.isPresent()) {
+            updatedAnimal.setAnimalid(id);
+            animalrepos.save(updatedAnimal);
+            return updatedAnimal;
+        } else {
+            return null;
+        }
+    }
 
 }
--- zoos/src/main/java/com/lambdaschool/zoos/controller/ZooController.java
+++ zoos/src/main/java/com/lambdaschool/zoos/controller/ZooController.java
@@ -19,6 +19,5 @@ public class ZooController {
         return zoorepos.findAll();
     }
 
-    @PutMapping
 
 }
